c559f927e990274843e326dc7ca8261c7788898d
Thai Nguyen <thainm.uet@gmail.com>
2023-06-28 23:19:00 +0700

    Catch exceptions

--- helpers.py
+++ helpers.py
@@ -4,22 +4,29 @@ from settings import DEFAULT_LANGUAGE, DICTIONARY_API_URL
 
 
 def get_word_information(language, word):
-  language if language == DEFAULT_LANGUAGE else DEFAULT_LANGUAGE 
-  r = requests.get(f'{DICTIONARY_API_URL}/{language}/{word}')
-  return r.json()
+  language if language == DEFAULT_LANGUAGE else DEFAULT_LANGUAGE
+  try:
+    r = requests.get(f'{DICTIONARY_API_URL}/{language}/{word}')
+  except requests.exceptions.ConnectionError:
+    print('Connection Error: Please check your internet connection.')
+  else:
+    return r.json()
+
 
 def aggregate_meanings(meanings):
   result = ''
-  
   for meaning in meanings:
     part_of_speech = meaning['partOfSpeech']
     definitions = [f'- {definition["definition"]}' for definition in meaning['definitions']]
     formated_def = '\n'.join(definitions)
     result += f'[{part_of_speech.upper()}]\n{formated_def}\n\n'
-  return result
+  else:
+    return result
+
 
 def extract_information(information):
-  meanings = aggregate_meanings(information['meanings'])
-  result = f"""Word: {information['word']}
+    meanings = aggregate_meanings(information['meanings'])
+    result = f"""---
+WORD: {information['word']}\n
 {meanings}"""
-  return result
+    return result
--- pydict.py
+++ pydict.py
@@ -1,18 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import argparse
-import helpers 
+import helpers
 
 from settings import DEFAULT_LANGUAGE
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(prog='PyDict', description='A Python commandline dictionary')
+  parser = argparse.ArgumentParser(prog='pydict', description='A Python commandline dictionary.')
   parser.add_argument('-l', '--language', default=DEFAULT_LANGUAGE, help='Language to search for.')
   parser.add_argument('word', help='Word to search for definition.')
-
-  args = parser.parse_args()
-  word_information = helpers.get_word_information(args.language, args.word)
-  result = helpers.extract_information(word_information[0])
   
-  print(result)
+  args = parser.parse_args()
+  
+  try:
+    word_information = helpers.get_word_information(args.language, args.word)
+    result = helpers.extract_information(word_information[0])
+  except KeyError:
+    print('No definition found.')
+  else:
+    print(result)
